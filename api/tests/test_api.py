57393fcb5df8caa0ab7dc4178adb89ab3c5429b7
Abdul-o <abdullateefdauda01@gmail.com>
2026-04-21 16:27:31 +0100

    Fix import structure

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,8 +1,5 @@
-import sys
-import os
 from fastapi.testclient import TestClient
 from api.main import app
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 
 client = TestClient(app)
