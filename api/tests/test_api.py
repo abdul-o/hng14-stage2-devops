2fb25cbbaf401388608c199f6911574a866c1a53
Abdul-o <abdullateefdauda01@gmail.com>
2026-04-21 16:40:22 +0100

    Fix import path

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,10 +1,5 @@
 from fastapi.testclient import TestClient
-import sys
-import os
-
-from main import app
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from api.main import app
 
 
 client = TestClient(app)
